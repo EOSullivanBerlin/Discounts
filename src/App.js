7deed4747d7c72c2f99801ab3fdcac511a90ebcc
EOSullivanBerlin <esully666@gmail.com>
2018-06-22 12:12:06 +0200

    Updated the react component to display the discounts

--- src/App.js
+++ src/App.js
@@ -1,30 +1,50 @@
 import React, { Component } from 'react';
 import './App.css';
 import DiscountDisplay from './components/DiscountDisplay';
-import DiscountFn from './discountFunctions/DiscountFunctions';
+import DiscountFunctions from './discountFunctions/DiscountFunctions';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       order: {
-        "id": "2",
-        "customer-id": "2",
-        "items": [
+        'id': '1',
+        'customer-id': '1',
+        'items': [
           {
-            "product-id": "B102",
-            "quantity": "5",
+            'id': 'A101',
+            'quantity': '6',
+            'unit-price': '9.75',
+            'total': '58.50',
+          },
+          {
+            'id': 'B103',
+            'quantity': '2',
+            'unit-price': '12.95',
+            'total': '25.90',
+          },
+          {
+            "id": "B102",
+            "quantity": "10",
             "unit-price": "4.99",
-            "total": "24.95"
-          }
+            "total": "49.90"
+          },
         ],
-        "total": "24.95"
-      }
+        'total': '134.30',
+      },
+      customer: {
+        "id": "2",
+        "name": "Teamleader",
+        "since": "2015-01-15",
+        "revenue": "1505.95"
+      },
     }
   }
 
-  handleDiscountThree(order) {
-    return DiscountFn.totalByTwo(order);
+  handleDiscounts(order, customer) {
+    let result = DiscountFunctions.Discounter(order, customer);
+    console.log(result.discounts.a.reduction);
+    return result.discounts;
   }
 
   render() {
@@ -37,7 +57,8 @@ class App extends Component {
         </p>
         <DiscountDisplay
         order={this.state.order}
-        DiscountThree={this.handleDiscountThree.bind(this)}
+        customer={this.state.customer}
+        Discount={this.handleDiscounts.bind(this)}
         />
       </div>
     );
--- src/components/DiscountDisplay.js
+++ src/components/DiscountDisplay.js
@@ -3,8 +3,14 @@ import React from 'react';
 function DiscountDisplay(props) {
   return (
     <div>
+      <p> Cutomer: {props.customer.id} </p>
       <p>Total: {props.order.total}</p>
       <p className='discount'>Discount: </p>
+      <p> Discount for purchasing over a thousand euro in products: {(props.Discount(props.order, props.customer)).c.reduction} </p>
+      <p> Discount for buying two type 1/A products: {(props.Discount(props.order, props.customer)).a.reduction} </p>
+      <p> Discount for buying six type 2/B products: {(props.Discount(props.order, props.customer)).b.reduction} </p>
+      <p> Net Discount: </p>
+      <p>Updated Total: </p>
     </div>
   );
 }
